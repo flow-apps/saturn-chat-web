c85d9aeb9e70e41b8e00e2a4d996040b7ae717de
pedrohenriquebraga <pedrobraga693@gmail.com>
2026-08-26 22:28:02 -0300

    fix images patterns

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   reactStrictMode: true,
+  // images: {
+  //   domains: ["192.168.0.108", "storage.googleapis.com/saturn-chat-618e5.appspot.com"]
+  // }
+
   images: {
-    domains: ["192.168.0.108", "storage.googleapis.com/saturn-chat-618e5.appspot.com"]
-  }
-}
+    remotePatterns: [
+      new URL("http://192.168.0.108"),
+      new URL("https://storage.googleapis.com/saturn-chat-618e5.appspot.com"),
+      new URL("https://saturnchat.azurewebsites.net/")
+    ],
+  },
+};
