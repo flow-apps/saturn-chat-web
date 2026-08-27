0189a52f8f5515a8291c28923a2214c7efc88114
pedrohenriquebraga <pedrobraga693@gmail.com>
2026-08-26 22:29:44 -0300

    fix deprecated

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  // images: {
-  //   domains: ["192.168.0.108", "storage.googleapis.com/saturn-chat-618e5.appspot.com"]
-  // }
-
   images: {
     remotePatterns: [
       new URL("http://192.168.0.108"),
--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const token = request.cookies.get("@SaturnChat:token")?.value;
 
   const { pathname } = request.nextUrl;
