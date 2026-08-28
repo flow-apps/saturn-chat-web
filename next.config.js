c08b0b0a653ce7d7ae6687cde5263898bc503212
pedrohenriquebraga <pedrobraga693@gmail.com>
2026-08-28 16:13:27 -0300

    add cors

--- next.config.js
+++ next.config.js
@@ -1,10 +1,33 @@
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: 'https://saturnchat.com.br' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS, DELETE, PATCH' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
-      new URL("http://192.168.0.108"),
-      new URL("https://storage.googleapis.com/saturn-chat-618e5.appspot.com"),
-      new URL("https://saturnchat.azurewebsites.net/files/**")
+      {
+        protocol: 'http',
+        hostname: '192.168.0.108',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '/saturn-chat-618e5.appspot.com/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'saturnchat.azurewebsites.net',
+        pathname: '/files/**',
+      },
     ],
   },
 };
